ec02f8509e1e58a408a29541d8f5f7a24879bcb5
OniqLoshiq <doncho.vasilev@gmail.com>
2018-10-04 23:06:51 +0300

    C# Web Basics - Exercise 4 Add Cookies and Session to SIS

---
+++ 09- 180917-C# Web Basics/181001-WB-9 Ex 4 - StateManagement/181001-WB-9 Ex 4/SIS/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using SIS.HTTP.Enums;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.WebServer.Results;
+
+namespace SIS
+{
+    public class HomeController
+    {
+        public IHttpResponse Index(IHttpRequest request)
+        {
+            string content = "<h1>Hello, Friend! How you doin'? Have a nice time!</h1>";
+
+            return new HtmlResult(content, HttpResponseStatusCode.Ok);
+        }
+    }
+}
